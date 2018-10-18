d78fea463fc9d7f8ba65944050fe6a1137c1b81f
Kira Kim <yein9718@gmail.com>
2018-10-18 12:18:27 +0100

    methods written in read_images.py to get artist oriented file system

--- process_csv.py
+++ process_csv.py
@@ -64,8 +64,28 @@ class ProcessCSV:
             i += 1
         return data
 
-    def generate_file_system(path):
-        # generate file system for artist class labels 
+    # https://www.bogotobogo.com/python/python_longest_common_substring_lcs_algorithm_generalized_suffix_tree.php
+    def lcs(S,T):
+        m = len(S)
+        n = len(T)
+        counter = [[0]*(n+1) for x in range(m+1)]
+        longest = 0
+        lcs_set = set()
+        for i in range(m):
+            for j in range(n):
+                if S[i] == T[j]:
+                c = counter[i][j] + 1
+                counter[i+1][j+1] = c
+                    if c > longest:
+                        lcs_set = set()
+                        longest = c
+                        lcs_set.add(S[i-c+1:i+1])
+                    elif c == longest:
+                        lcs_set.add(S[i-c+1:i+1])
+
+        return lcs_set
+
+    def
 
 # df.loc[df.index[#], 'NAME']
 
--- read_images.py
+++ read_images.py
@@ -1,4 +1,4 @@
-import os
+import shutil, os
 
 class ImageDataGenerator:
     # preprocessing required for wikipaintings dataset
@@ -28,15 +28,57 @@ class ImageDataGenerator:
                 f.write(os.path.abspath(file)+" "+str(lines.index(dir))+"\n")
         f.close()
 
-    def generate_artist_file_system(path):
-        
+#for index, item in enumerate(my_list):
+#    print index, item
+
+#If you only need the indices, you can use range():
+
+#for i in range(len(my_list)):
+#    print i
+from distutils.dir_util import copy_tree
+
+# copy subdirectory example
+fromDirectory = "/a/b/c"
+toDirectory = "/x/y/z"
+
+copy_tree(fromDirectory, toDirectory)
+
+    def generate_artist_file_system(path, dest_path):
+        if os.path.exists(dest_path):
+            shutil.rmtree(dest_path)
+        os.mkdir(dest_path)
+        upper_dirs = os.listdir(path)
+        for mid_dir in upper_dirs:
+            #os.mkdir(os.path.join(dest_path, mid_dir))
+            #dest_mid_dir = os.path.join(dest_path, mid_dir)
+            dirs = os.listdir(os.path.join(path, mid_dir))
+
+            for dir in dirs: # for every class dir
+                files = os.listdir(os.path.join(path, mid_dir, dir))
+
+                for index, item in enumerate(files): # file in files:
+
+                    dest_files = os.listdir(dest_path)#dest_mid_dir)
+                    current_item_path = os.path.join(path, mid_dir, dir, item)
+                    new_name = item.split('_')[0]
+                    if not new_name in dest_files:
+                        os.mkdir(os.path.join(dest_path, new_name))#dest_mid_dir, new_name))
+                    shutil.copy(current_item_path, os.path.join(dest_path, new_name, item))#dest_mid_dir, new_name, item))
+        print("File system created under data")
+
 
 if __name__ == '__main__':
-    path = "../../../../../scratch/yk30/wikipaintings_full/wikipaintings_train"
-    name = "wikipaintings_class_labels.txt"
-    ImageDataGenerator.enumerate_class_names(name,path)
-    data_file_name = "train.txt"
-    ImageDataGenerator.generate_load_image_file(data_file_name, path, name)
-    path_val = "../../../../../scratch/yk30/wikipaintings_full/wikipaintings_val"
-    val_file_name = "val.txt"
-    ImageDataGenerator.generate_load_image_file(val_file_name, path_val, name)
+    #path = "../../../../../scratch/yk30/wikipaintings_full/wikipaintings_train"
+    #name = "wikipaintings_class_labels.txt"
+    #ImageDataGenerator.enumerate_class_names(name,path)
+    #data_file_name = "train.txt"
+    #ImageDataGenerator.generate_load_image_file(data_file_name, path, name)
+    #path_val = "../../../../../scratch/yk30/wikipaintings_full/wikipaintings_val"
+    #val_file_name = "val.txt"
+    #ImageDataGenerator.generate_load_image_file(val_file_name, path_val, name)
+    #path = "./wikipaintings_small"
+    #dest_path = "./data/wikipaintings_artist"
+
+    path = "../../../../scratch/yk30/wikipaintings_full"
+    dest_path = "../../../../scratch/yk30/wikipaintings_full_artist"
+    ImageDataGenerator.generate_artist_file_system(path, dest_path)
