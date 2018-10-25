a96a59bbdcd20c2488d34dcf29b80e09b500d5e8
Kira Kim <yk30@st-andrews.ac.uk>
2018-10-25 09:52:59 +0100

    tidying up the space

--- cleaning/prepare_data.py
+++
@@ -1,8 +0,0 @@
-import imageio
-# x = load_img(join(DATA_PATH,folder,file),target_size=(224,224))
-print('Printing one from IDB...')
-im2 = imageio.imread('../IDB-115-072827_fullsize.jpg')
-print(im2)
-
-class Prepare:
-    def save_
