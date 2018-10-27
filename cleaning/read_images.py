9a86d5e93201ee7b2a6d96767a2011908d292c78
Yein Kim <y_kim15@hotmail.com>
2018-10-27 16:48:41 +0100

    read_images changed to only have methods and more requirements added, rasta-master renamed as rasta, wp full dataset downloaded in rasta-data

--- cleaning/read_images.py
+++ cleaning/read_images.py
@@ -1,93 +1,117 @@
 import shutil, os
+from os.path import dirname, abspath
 import pandas
 from clean_csv import Clean
+import imageio
+from keras.preprocessing.image import load_img
+_NUM_SAMPLES = 1000
 
-class ImageDataGenerator:
 
-    def get_image_matrix(file_path, id, col_name):
-        # read train_data_small type file, find the row with matching id
-        # get the image and return the matrix form of the image (np format)
-        # col_name : col name of where the path exists
-        df = pandas.read_csv(file_path)
-        row = df.loc[df["id"] == str(id)]
-        path = row[col_name]
-        im = imageio.imread(path)
-        return im
+def get_input_data(file_path):
+    # assume file has form id, path, label
+    df = pandas.read_csv(file_path)
+    new_df = pandas.DataFrame(columns = ["images"])
+    new_label = pandas.DataFrame(columns = ["label"])
+    i = 0
+    for index, row in df.iterrows():
+        im = load_img(row["path"], target_size=(224,224))
+        print(type(im))
+        im = imageio.imread(row["path"])
+        print(type(im))
+        new_df.loc[i] = im
+        new_label.loc[i] = row["label"]
+        i += 1
+        break
+        #new_df = new_df.append(pandas.Series([im], index = str(row["id"])))
+        #new_label = new_label.append(pandas.Series([row["label"]], index = str(row["id"])))
+    return (new_df.values, new_label.values)
 
-    # preprocessing required for wikipaintings dataset
 
-    def enumerate_class_names(name, path):
-        # given path to the directory, writes all names of class labels to a file.
-        # overwrite if exists else create one : "w+"
-        f = open(name, "w+")
-        dirs = sorted(os.listdir(path))
-        i = 0
-        for file in dirs:
-            f.write(file+"\n")
+def get_image_matrix(file_path, id, col_name):
+    # read train_data_small type file, find the row with matching id
+    # get the image and return the matrix form of the image (np format)
+    # col_name : col name of where the path exists
+    df = pandas.read_csv(file_path)
+    row = df.loc[df["id"] == str(id)]
+    path = row[col_name]
+    im = imageio.imread(path)
+    return im
+
+# preprocessing required for wikipaintings dataset
+def enumerate_class_names(name, path):
+    # given path to the directory, writes all names of class labels to a file.
+    # overwrite if exists else create one : "w+"
+    f = open(name, "w+")
+    dirs = sorted(os.listdir(path))
+    i = 0
+    for file in dirs:
+        f.write(file+"\n")
+        i += 1
+    f.close()
+
+def generate_image_id_file(name, path, class_file_path):
+    # class_file_path: path where class label file is
+    # path: path of parent directory containing all data (e.g. wikipaintings_train)
+    # name: path of output file
+    # generates file with list of absolute path of images with its class label as csv with code included
+    # class label order indexed from 0 determined from class_label.txt file generated
+
+    class_f = open(class_file_path, "r")
+    lines = class_f.read().split('\n')
+    headers = ["id", "path", "label", "class_name"]
+    new_f = pandas.DataFrame(columns = headers)
+
+    dirs = sorted(os.listdir(path))
+    i = 0
+    for dir in dirs:
+        files = sorted(os.listdir(os.path.join(path, dir)))
+        for file in files:
+            file_path = os.path.join("..","wikipaintings_small", "wikipaintings_train", dir, file)
+            new_f.loc[i] = ["NaN", file_path, str(lines.index(dir)), str(dir)]
             i += 1
-        f.close()
 
-    def generate_image_id_file(name, path, class_file_path):
-        # class_file_path: path where class label file is
-        # path: path of parent directory containing all data (e.g. wikipaintings_train)
-        # name: path of output file
-        # generates file with list of absolute path of images with its class label as csv with code included
-        # class label order indexed from 0 determined from class_label.txt file generated
+    new_f = Clean.assign_id(new_f, "label")
+    new_f.to_csv(name, header=headers, index=False)
 
-        class_f = open(class_file_path, "r")
-        lines = class_f.read().split('\n')
-        headers = ["id", "path", "label"]
-        new_f = pandas.DataFrame(columns = headers)
+def generate_artist_file_system(path, dest_path):
+    if os.path.exists(dest_path):
+        shutil.rmtree(dest_path)
+    os.mkdir(dest_path)
+    upper_dirs = os.listdir(path)
+    for mid_dir in upper_dirs:
+        #os.mkdir(os.path.join(dest_path, mid_dir))
+        #dest_mid_dir = os.path.join(dest_path, mid_dir)
+        dirs = os.listdir(os.path.join(path, mid_dir))
 
-        dirs = sorted(os.listdir(path))
-        i = 0
-        for dir in dirs:
-            files = sorted(os.listdir(os.path.join(path, dir)))
-            for file in files:
-                file_path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)), "wikipaintings_small", "wikipaintings_train", file)
-                new_f.loc[i] = ["NaN", file_path, str(lines.index(dir))]
-                i += 1
+        for dir in dirs: # for every class dir
+            files = os.listdir(os.path.join(path, mid_dir, dir))
 
-        new_f = Clean.assign_id(new_f, "label")
-        new_f.to_csv(name, header=headers, index=False)
-
-    def generate_artist_file_system(path, dest_path):
-        if os.path.exists(dest_path):
-            shutil.rmtree(dest_path)
-        os.mkdir(dest_path)
-        upper_dirs = os.listdir(path)
-        for mid_dir in upper_dirs:
-            #os.mkdir(os.path.join(dest_path, mid_dir))
-            #dest_mid_dir = os.path.join(dest_path, mid_dir)
-            dirs = os.listdir(os.path.join(path, mid_dir))
-
-            for dir in dirs: # for every class dir
-                files = os.listdir(os.path.join(path, mid_dir, dir))
-
-                for index, item in enumerate(files): # file in files:
-
-                    dest_files = os.listdir(dest_path)#dest_mid_dir)
-                    current_item_path = os.path.join(path, mid_dir, dir, item)
-                    new_name = item.split('_')[0]
-                    if not new_name in dest_files:
-                        os.mkdir(os.path.join(dest_path, new_name))#dest_mid_dir, new_name))
-                    shutil.copy(current_item_path, os.path.join(dest_path, new_name, item))#dest_mid_dir, new_name, item))
-        print("File system created under data")
+            for index, item in enumerate(files): # file in files:
+                dest_files = os.listdir(dest_path)#dest_mid_dir)
+                current_item_path = os.path.join(path, mid_dir, dir, item)
+                new_name = item.split('_')[0]
+                if not new_name in dest_files:
+                    os.mkdir(os.path.join(dest_path, new_name))#dest_mid_dir, new_name))
+                shutil.copy(current_item_path, os.path.join(dest_path, new_name, item))#dest_mid_dir, new_name, item))
+    print("File system created under data")
 
 
 if __name__ == '__main__':
     #path = "../../../../../scratch/yk30/wikipaintings_full/wikipaintings_train"
     path = "../wikipaintings_small/wikipaintings_train"
     name = "../data/wikipaintings_class_labels_small.txt"
-    ImageDataGenerator.enumerate_class_names(name,path)
+    enumerate_class_names(name,path)
     data_file_name = "../data/train_data_small.txt"
-    ImageDataGenerator.generate_image_id_file(data_file_name, path, name)
+    generate_image_id_file(data_file_name, path, name)
+    x, y = get_input_data(data_file_name)
+    print(type(x), type(y))
+    print(x.shape, y.shape)
     #path_val = "../../../../../scratch/yk30/wikipaintings_full/wikipaintings_val"
     #val_file_name = "val.txt"
-    #ImageDataGenerator.generate_load_image_file(val_file_name, path_val, name)
+    #generate_load_image_file(val_file_name, path_val, name)
     #path = "./wikipaintings_small"
     #dest_path = "./data/wikipaintings_artist"
 
     #path = "../../../../scratch/yk30/wikipaintings_full"
     #dest_path = "../../../../scratch/yk30/wikipaintings_full_artist"
-    #ImageDataGenerator.generate_artist_file_system(path, dest_path)
+    #generate_artist_file_system(path, dest_path)
