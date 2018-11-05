19e87a393267bbb45737ff9dabdb74178caf4a59
Yein Kim <y_kim15@hotmail.com>
2018-11-05 21:23:26 +0000

    currently testing to vgg model. changed the design of building new model from pretrained model by creating a new model instance for the top layers

--- fine_tune.py
+++ fine_tune.py
@@ -2,7 +2,7 @@ from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Flatten, Dropout
+from tensorflow.keras.layers import Input, Dense, GlobalAveragePooling2D, Flatten, Dropout
 from rasta.python.utils.utils import wp_preprocess_input
 from rasta.python.models.processing import count_files
 from contextlib import redirect_stdout
@@ -10,25 +10,39 @@ import datetime
 import os
 
 dirname = os.path.dirname(__file__)
+# Code influenced by keras application examples
+# that from rasta and from ...
 
 def get_vgg16(input_shape, n_classes, pretrained=True):
+    input = Input(shape=input_shape)
     if not pretrained:
         base_model = VGG16(include_top=True, weights='None', input_shape=input_shape)
         return base_model
     else:
-        base_model = VGG16(include_top=False, weights='imagenet', input_shape=input_shape)
+        base_model = VGG16(include_top=True, weights='imagenet', input_shape=input_shape)
 
-    x = base_model.output
-    #x = GlobalAveragePooling2D()(x)
-    x = Flatten()(x)
-    x = Dense(4096, activation='relu')(x)
-    x = Dropout(0.5)(x)
-    x = Dense(4096, activation='relu')(x)
-    x = Dropout(0.5)(x)
-    output = Dense(n_classes, activation='softmax')(x)
-    
+    x = Sequential()
+    #x.add(Flatten(input_shape=base_model.output_shape[1:]))
+    #x.add(Dense(512, activation='relu'))
+    #x.add(Dropout(0.5))
+    #x.add(Dense(256, activation='relu'))
+    #x.add(Dropout(0.5))
+    #x.add(Dense(n_classes, activation='softmax'))
+    return base_model, x
 
-    return base_model, output
+    #model = Model(input=base_model.input, output=x(base_model.output))
+
+    #x = base_model.output
+    #x = Dense(4096, activation='relu')(x)
+    #x = Dropout(0.5)(x)
+    #x = Dense(4096, activation='relu')(x)
+    #x = Dropout(0.5)(x)
+    #output = Dense(n_classes, activation='softmax')(x)
+    #model.outputs = [model.layers[-1].output]
+    #model.layers[-1].outbound_nodes = []
+    #model.add(Dense(num_class, activation='softmax'))
+
+    #return base_model, x
 
 def get_inceptionv3(input_shape, n_classes, pretrained=True):
     if not pretrained:
@@ -74,9 +88,15 @@ def finetune_model(model_type, input_shape, n_classes, n_tune_layers, train_path
     # tune output layer
     base_model, output = get_model[model_type](input_shape, n_classes, pretrained=True)
     train_generator = get_generator(train_path, batch_size, target_size=(input_shape[0], input_shape[1]), horizontal_flip=horizontal_flip)
-    model = Model(inputs=base_model.input, outputs=output)
-
-    for layer in base_model.layers:
+    model = Model(inputs=base_model.input, outputs=base_model.output)#output(base_model.output))#output)
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy',metrics=['accuracy'])
+    name = get_model_name(model_type)
+    if save:
+        save_summary(name, model)
+    """
+    #for layer in base_model.layers:
+    #    layer.trainable = False
+    for layer in model.layers[:13]:
         layer.trainable = False
 
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy',metrics=['accuracy'])
@@ -85,6 +105,7 @@ def finetune_model(model_type, input_shape, n_classes, n_tune_layers, train_path
         save_summary(name, model)
     model.fit_generator(train_generator, steps_per_epoch=count_files(train_path)//batch_size, epochs=epochs)
     val_generator = get_generator(val_path, batch_size, target_size=(input_shape[0], input_shape[1]), horizontal_flip=horizontal_flip)
+"""
     # either make a separate function tune_layers so just write it here
     return
 
--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 from fine_tune import *
+import tensorflow as tf
+
+config = tf.ConfigProto(allow_soft_placement=True)
+config.gpu_options.allocator_type = 'BFC'
+#config.gpu_options.per_process_gpu_memory_fraction = 0.40
+config.gpu_options.allow_growth = True
+
 
 TRAIN_PATH="rasta/data/wikipaintings_full/wikipaintings_train"
 VAL_PATH="rasta/data/wikipaintings_full/wikipaintings_val"
 
 N_CLASSES=25
-BATCH_SIZE=10
-finetune_model(model_type="vgg16", input_shape=(224,244,3), n_classes=N_CLASSES, n_tune_layers=1, train_path=TRAIN_PATH,
+BATCH_SIZE=50
+finetune_model(model_type="vgg16", input_shape=(224,224,3), n_classes=N_CLASSES, n_tune_layers=1, train_path=TRAIN_PATH,
     val_path=VAL_PATH, horizontal_flip=True, batch_size=BATCH_SIZE)
